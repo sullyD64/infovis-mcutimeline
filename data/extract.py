628cbc0304272d499aa9c7d05bb417f2e4c7781b
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2020-01-20 15:08:10 +0100

    [Data] Refactor extraction into method chaining class.

--- data/extract.py
+++ data/extract.py
@@ -1,84 +1,147 @@
+import glob
 import json
 import os
 import re
-from html.parser import HTMLParser
 
-from parse import Ref
+from parse import Event, Ref
 
-refs_movie = ['IM', 'TIH', 'IM2', 'T', 'CATFA', 'TA', 'IM3', 'TTDW', 'CATWS', 'GotG', 'AAoU',
+refnames_movie = ['IM', 'TIH', 'IM2', 'T', 'CATFA', 'TA', 'IM3', 'TTDW', 'CATWS', 'GotG', 'AAoU',
               'AM', 'CACW', 'DS', 'GotGv2', 'SMH', 'TR', 'BP', 'AIW', 'AMatW', 'CM', 'AE', 'SMFFH', ]
 
-refs_shows = ['AoS', 'AC', 'DD', 'JJ', 'LC', 'IF', 'TD', 'TP', 'R', 'C&D', ]
+refnames_shows = ['AoS', 'AC', 'DD', 'JJ', 'LC', 'IF', 'TD', 'TP', 'R', 'C&D', ]
 
 
+class Extractor(object):
+    """ 
+    An Extractor maintains a list of elements, which can be subsequentially manipulated.
+    """
 
-# def enrich_refs(refs):
+    def __init__(self, infile=None, data=None):
+        if data:
+            self.data = data
+        else:
+            with open(os.path.join(os.path.dirname(__file__), f'{infile}.json')) as wrapper:
+                self.data = json.loads(wrapper.read())
 
+    def fork(self):
+        """
+        Returns a new copy of the Extractor, already initialized to the current state.
+        """
+        return Extractor(data=self.data)
 
+    def load_events(self):
+        """
+        Parses the raw data into Event and Ref objects.
+        Subsequent operations are applied to those classes and make use of attributes instead of dict keys.
+        """
+        self.data = [Event.from_dict(**d) for d in self.data]
+        return self
+
+    def filter_row(self, pred):
+        """ 
+        Filters data by selecting elements matching pred.
+        """
+        if callable(pred):
+            self.data = list(filter(pred, self.data))
+        else:
+            raise NotImplementedError
+        return self
+
+    def filter_col(self, col_names: list):
+        """
+        Filters data by selecting the given attributes or keys for each element in data.
+        Works with both objects and dicts, replaces elements with dicts containing the selected columns.
+        """
+        if isinstance(self.data[0], object):
+            self.data = [{col: getattr(d, col) for col in col_names} for d in self.data]
+        elif isinstance(self.data[0], dict):
+            self.data = [{col: d[col] for col in col_names} for d in self.data]
+        else:
+            raise NotImplementedError
+        return self
+
+    def consume_key(self, key: str):
+        """
+        ** Must be called after filter_col **
+        If data elements are dicts and key is in the dicts' keysets, "consumes" the key.
+        Replaces elements with the key's corresponding values.
+        """
+        keys = self.data[0].keys()
+        if key in keys:
+            self.data = [d[key] for d in self.data]
+        else:
+            raise KeyError(key)
+        return self
+
+    def flatten(self):
+        """Flattens data (a list of lists) into a list."""
+        self.data = [x for sublist in self.data for x in sublist]
+        return self
+
+    def sort(self):
+        """Sorts data."""
+        self.data = sorted(self.data)
+        return self
+
+    def unique(self):
+        """Removes duplicates from data."""
+        self.data = list(set(self.data))
+        return self
+
+    def output(self, outpath=None):
+        def __dictify(elem):
+            """Ready data for dumping."""
+            if isinstance(elem, (Event, Ref)):
+                return elem.to_dict()
+            elif isinstance(elem, dict):
+                for k, v in elem.items():
+                    elem[k] = __dictify(v)
+            elif isinstance(elem, list):
+                elem = [__dictify(x) for x in elem]
+            return elem
+
+        outpath = '' if not outpath else outpath
+        with open(os.path.join(os.path.dirname(__file__), f'extracted__{outpath}.json'), 'w') as outfile:
+            dict_data = list(map(__dictify, self.data))
+            outfile.write(json.dumps(dict_data, indent=2, ensure_ascii=False))
+        return self
 
 
 if __name__ == "__main__":
-    inpath = os.path.join(os.path.dirname(__file__), 'parsed.json')
-    outpath = os.path.join(os.path.dirname(__file__), 'extracted.json')
+    infile = 'parsed'
 
-    with open(inpath) as wrapper:
-        data = json.loads(wrapper.read())
-        
-        # filter event columns
-        selected_columns = [
-            {
-                'id': d['id'],
-                # 'file': d['file'],
-                # 'line': d['line'],
-                # 'date': d['date'],
-                # 'reality': d['reality'],
-                # 'title': d['title'],
-                # 'desc': d['desc'],
-                # 'multiple': d['multiple']
-                # 'links': d['links'],
-                'refs': d['refs'],
-            } for d in data
-        ]   
-        events = selected_columns
-        
-        # filter event rows
-        # events = list(filter(lambda ev: ev['reality'] == 'Sakaar', events))
-        # events = list(filter(lambda ev: ev['multiple'], events))
-        # events = list(filter(lambda ev: ev['id'] == 9842, selected_columns))
+    for outfile in glob.glob(os.path.join(os.path.dirname(__file__), 'extracted__*')):
+        pass
+        # os.remove(outfile)
 
-        
-        events_refs = [ev['refs'] for ev in data]  # refs extraction
-        refset = [ref for sublist in events_refs for ref in sublist]  # flatten to a single list of refs
+    refs = Extractor(infile)
+    (refs
+        .load_events()
+        # .filter_row(lambda ev: ev.file == "1900s")
+        .filter_col(['refs'])
+        .consume_key('refs')
+        .flatten()
+        .output()
+    )
 
-        selected_columns_refs = [
-            Ref.from_dict(**d) for d in refset
-        ]
-        refs = selected_columns_refs
+    # extract anonymous refs
+    refs_anon = (refs.fork()
+        .filter_row(lambda ref: ref.ref_name is None)
+        .filter_col(['eid', 'ref_desc', 'ref_links'])
+        .output('refs_anon')
+    )
 
-        # extract non-named refs:   
-        anon_refs = list(filter(lambda x: x.ref_name is None, refs))
+    # extract named refs
+    refs_named = (refs.fork()
+        .filter_row(lambda ref: ref.ref_name is not None)
+        .output('refs_named')
+    )
 
-        # extract named refs:
-        named_refs = list(filter(lambda x: x.ref_name is not None, refs))
-
-        # extract unique refnames
-        ref_names = [x.ref_name for x in named_refs]
-        
-        # out_data = anon_refs
-        out_data = named_refs
-        # out_data = sorted(list(set(ref_names)))
-        # out_data = refs
-
-    with open(outpath, 'w') as outfile:
-
-        out_dict_data = [ref.__dict__ for ref in out_data]
-        # out_dict_data = [
-        #     {
-        #         # 'eid': d['eid'],
-        #         'ref_name': d['ref_name'],
-        #         'ref_desc': d['ref_desc'],
-        #         'ref_links': d['ref_links'],
-        #     } for d in out_dict_data
-        # ]
-
-        outfile.write(json.dumps(out_dict_data, indent=2, ensure_ascii=False))
+    # extract unique refnames
+    refnames = (refs_named.fork()
+        .filter_col(['ref_name'])
+        .consume_key('ref_name')
+        .unique()
+        .sort()
+        .output('refnames')
+    )
