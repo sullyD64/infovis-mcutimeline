6a70259271d640255cbbc50fea8f63ac5863758f
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2020-01-20 19:35:24 +0100

    [Data] Add mapto and addattr to Extractor, improve refs extraction

--- data/extract.py
+++ data/extract.py
@@ -5,10 +5,8 @@ import re
 
 from parse import Event, Ref
 
-refnames_movie = ['IM', 'TIH', 'IM2', 'T', 'CATFA', 'TA', 'IM3', 'TTDW', 'CATWS', 'GotG', 'AAoU',
-              'AM', 'CACW', 'DS', 'GotGv2', 'SMH', 'TR', 'BP', 'AIW', 'AMatW', 'CM', 'AE', 'SMFFH', ]
-
-refnames_shows = ['AoS', 'AC', 'DD', 'JJ', 'LC', 'IF', 'TD', 'TP', 'R', 'C&D', ]
+# refnames_movie = ['IM', 'TIH', 'IM2', 'T', 'CATFA', 'TA', 'IM3', 'TTDW', 'CATWS', 'GotG', 'AAoU', 'AM', 'CACW', 'DS', 'GotGV2', 'SMH', 'TR', 'BP', 'AIW', 'AMatW', 'CM', 'AE', 'SMFFH', ]
+# refnames_shows = ['AoS', 'AC', 'AoSSS', 'I', 'DD', 'JJ', 'LC', 'IF', 'TD', 'TP', 'R', 'C&D', ]
 
 
 class Extractor(object):
@@ -17,7 +15,7 @@ class Extractor(object):
     """
 
     def __init__(self, infile=None, data=None):
-        if data:
+        if not infile:
             self.data = data
         else:
             with open(os.path.join(os.path.dirname(__file__), f'{infile}.json')) as wrapper:
@@ -29,13 +27,11 @@ class Extractor(object):
         """
         return Extractor(data=self.data)
 
-    def load_events(self):
+    def get(self):
         """
-        Parses the raw data into Event and Ref objects.
-        Subsequent operations are applied to those classes and make use of attributes instead of dict keys.
+        Returns current extractor data.
         """
-        self.data = [Event.from_dict(**d) for d in self.data]
-        return self
+        return self.data
 
     def filter_row(self, pred):
         """ 
@@ -52,10 +48,12 @@ class Extractor(object):
         Filters data by selecting the given attributes or keys for each element in data.
         Works with both objects and dicts, replaces elements with dicts containing the selected columns.
         """
-        if isinstance(self.data[0], object):
-            self.data = [{col: getattr(d, col) for col in col_names} for d in self.data]
-        elif isinstance(self.data[0], dict):
+        if not self.data:
+            raise Exception("filter_col: Empty list")
+        if isinstance(self.data[0], dict):
             self.data = [{col: d[col] for col in col_names} for d in self.data]
+        elif isinstance(self.data[0], object):
+            self.data = [{col: getattr(d, col) for col in col_names} for d in self.data]
         else:
             raise NotImplementedError
         return self
@@ -66,12 +64,32 @@ class Extractor(object):
         If data elements are dicts and key is in the dicts' keysets, "consumes" the key.
         Replaces elements with the key's corresponding values.
         """
+        if not self.data:
+            raise Exception("consume_key: Empty list")
         keys = self.data[0].keys()
         if key in keys:
             self.data = [d[key] for d in self.data]
         else:
             raise KeyError(key)
         return self
+
+    def mapto(self, func):
+        """Applies func to all elements in data."""
+        self.data = list(map(func, self.data))
+        return self
+
+    def addattr(self, attr, value):
+        """
+        Updates all elements, adding attribute `attr` with value `value`.
+        If elements are dicts, adds (`attr`,`value`) kv-pair instead.
+        """
+        for elem in self.data:
+            if isinstance(elem, dict):
+                elem[attr] = value
+            else:
+                elem.attr = value
+        return self
+
 
     def flatten(self):
         """Flattens data (a list of lists) into a list."""
@@ -84,13 +102,25 @@ class Extractor(object):
         return self
 
     def unique(self):
-        """Removes duplicates from data."""
-        self.data = list(set(self.data))
+        """If elements are hashable (aka they aren't dicts), removes duplicate elements."""
+        if not isinstance(self.data[0], dict):
+            self.data = list(set(self.data))
+        else:
+            raise NotImplementedError
         return self
 
-    def output(self, outpath=None):
+    def count(self, what: str):
+        """Prints the number of current elements"""
+        print(f'{len(self.data)} {what}')
+        return self
+
+    def save(self, outpath=None):
+        """
+        Saves current data outpath, serialized as JSON. Default file name is extracted__{outpath}.json. 
+        Python objects anywhere in data are dictified. (see Event.to_dict() or Ref.to_dict())
+        """
         def __dictify(elem):
-            """Ready data for dumping."""
+            """Readies data before dumping."""
             if isinstance(elem, (Event, Ref)):
                 return elem.to_dict()
             elif isinstance(elem, dict):
@@ -112,36 +142,63 @@ if __name__ == "__main__":
 
     for outfile in glob.glob(os.path.join(os.path.dirname(__file__), 'extracted__*')):
         pass
-        # os.remove(outfile)
+        os.remove(outfile)
 
-    refs = Extractor(infile)
-    (refs
-        .load_events()
+    extr_refs = Extractor(infile)
+
+    # extract all refs, flattened
+    (extr_refs
+        .mapto(lambda ev: Event.from_dict(**ev))  # Parse raw data into Event and Ref objects.
         # .filter_row(lambda ev: ev.file == "1900s")
+        # .filter_row(lambda ev: any([ref.ref_name == "\u0001" for ref in ev.refs]))
+        # .save('events')
         .filter_col(['refs'])
         .consume_key('refs')
         .flatten()
-        .output()
+        # .save('refs')
+     )
+
+    # extract unique anonymous refs
+    (extr_refs.fork()
+        .filter_row(lambda ref: ref.ref_name is None)
+        .unique()
+        .sort()
+        .filter_col(['eid', 'ref_desc', 'ref_links'])
+        .count('anonymous refs')
+        .save('refs_anon')
     )
 
-    # extract anonymous refs
-    refs_anon = (refs.fork()
-        .filter_row(lambda ref: ref.ref_name is None)
-        .filter_col(['eid', 'ref_desc', 'ref_links'])
-        .output('refs_anon')
+    extr_refs_named = (extr_refs.fork()
+        .filter_row(lambda ref: ref.ref_name is not None)
+        .sort()
+        .addattr('movie', 'provaprova') # TODO
     )
 
     # extract named refs
-    refs_named = (refs.fork()
-        .filter_row(lambda ref: ref.ref_name is not None)
-        .output('refs_named')
+    (extr_refs_named.fork()
+        .unique()
+        .sort()
+        .count('named refs')
+        .save('refs_named')
     )
 
     # extract unique refnames
-    refnames = (refs_named.fork()
+    refnames = (extr_refs_named.fork()
         .filter_col(['ref_name'])
         .consume_key('ref_name')
         .unique()
         .sort()
-        .output('refnames')
+        .count('refnames')
+        .save('refnames')
+        .get()
+    )
+
+    extr_movies = Extractor('movies')
+    extr_episodes = Extractor('episodes')
+
+    episodes_refnames = (extr_episodes
+        .fork()
+        .filter_col(['series', 'refname'])
+        .consume_key('refname')
+        .get()
     )
