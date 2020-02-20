2a540f64b7eb0b1c5bfacec434bf59dc119ae33d
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2020-02-20 23:44:02 +0100

    Add django models and admin

--- mcu_app/admin.py
+++ mcu_app/admin.py
@@ -1,1 +1,35 @@
+# mcu_app/admin.py
 from django.contrib import admin
+
+from mcu_app.models import Source, Event, Ref, Reflink
+
+class SourceAdmin(admin.ModelAdmin):
+    def events_count(self, obj):
+        return obj.event_set.count()
+    fieldsets = [
+        (None, {'fields': ['sid', 'title', 'type']}),
+        ('Details', {'fields': ['details_formatted']}),
+    ]
+    list_display = ('sid', 'title', 'type', 'details', 'events_count')
+    readonly_fields = ('details', 'details_formatted')
+
+
+class EventAdmin(admin.ModelAdmin):
+    def sources_count(self, obj):
+        return obj.sources.count()
+    def reflinks_count(self, obj):
+        return obj.reflink_set.count()
+    list_display = ('eid', 'filename', 'date', 'reality', 'title', 'desc', 'sources_count', 'reflinks_count')
+    
+
+class RefAdmin(admin.ModelAdmin):
+    list_display = ('rid', 'name', 'desc', 'source')
+
+
+class ReflinkAdmin(admin.ModelAdmin):
+    list_display = ('lid', 'evt', 'src', 'ref')
+
+admin.site.register(Source, SourceAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Ref, RefAdmin)
+admin.site.register(Reflink, ReflinkAdmin)
--- mcu_app/models.py
+++ mcu_app/models.py
@@ -1,2 +1,67 @@
 # mcu_app/models.py
+import json
+
 from django.db import models
+from django.utils.safestring import mark_safe
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers.data import JsonLexer
+
+
+class Source(models.Model):
+    sid = models.CharField(max_length=8, primary_key=True)
+    title = models.CharField(max_length=100, null=True)
+    type = models.CharField(max_length=30)
+    details = models.TextField()
+
+    def details_formatted(self):
+        json_obj = json.loads(self.details)
+        data = json.dumps(json_obj, indent=2)
+        formatter = HtmlFormatter(style='colorful')
+        response = highlight(data, JsonLexer(), formatter)
+        style = "<style>" + formatter.get_style_defs() + "</style><br/>"
+        return mark_safe(style + response)
+
+    def __str__(self):
+        return self.sid
+
+
+class Event(models.Model):
+    eid = models.CharField(max_length=8, primary_key=True)
+    filename = models.CharField(max_length=30)
+    line = models.CharField(max_length=30)
+    date = models.CharField(max_length=100)
+    reality = models.CharField(max_length=30)
+    title = models.CharField(max_length=200, null=True)
+    desc = models.TextField()
+    multiple = models.BooleanField(default=False)
+
+    sources = models.ManyToManyField(Source)
+   
+    # characters = models.TextField(blank=True) # TODO characters
+    # non_characters = models.TextField()
+    # ref_special # TODO ref_special
+
+    def __str__(self):
+        return self.eid
+
+
+class Ref(models.Model):
+    rid = models.CharField(max_length=8, primary_key=True)
+    name = models.CharField(max_length=30, null=True)
+    desc = models.TextField()
+
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.rid
+
+
+class Reflink(models.Model):
+    lid = models.CharField(max_length=8, primary_key=True)
+    evt = models.ForeignKey(Event, on_delete=models.CASCADE)
+    src = models.ForeignKey(Source, on_delete=models.CASCADE)
+    ref = models.ForeignKey(Ref, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.lid} ({self.evt} - {self.src} - {self.ref})'
