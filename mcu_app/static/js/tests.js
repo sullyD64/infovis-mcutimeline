2967429f959016b6218ba5b691b62b18b82f9d09
Lorenzo Guidaldi <lorenzo.guidaldi@fhoster.com>
2020-02-24 20:27:24 +0100

    Small tweaks to front end

--- mcu_app/static/js/tests.js
+++ mcu_app/static/js/tests.js
@@ -1,7 +1,22 @@
-svg = d3.select("main")
-  .append("svg")
-  .classed("canvas", true)
-  .classed("bg-white", true)
-  .attr("width", "100%")
-  .attr("height", "100%")
+
+var margin = {
+  top: window.innerHeight * 0.3,
+  bottom: window.innerHeight * 0.4,
+  right: 50,
+  left: 50,
+}
+
+var height = window.innerHeight - margin.top - margin.bottom;
+var screenWidth = window.innerWidth - margin.left - margin.right;
+
+
+svg = d3.select('main')
+  .append('svg')
+  .attr('width', screenWidth + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  // .attr('class', 'bg-white')
+  .append('g')  
+  .attr('class', 'chart')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
   // .attr("viewBox","0 0 1 1")
